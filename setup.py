4123979ad9ede2ba1a4ea52436c90ae14bd7e2fd
Clayton Davis <clayadavis@gmail.com>
2015-12-11 00:06:42 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='botornot',
+      version='0.1',
+      description='Check Twitter accounts for bot behavior',
+      url='https://github.com/truthy/botornot-python',
+      author='Clayton A Davsi',
+      author_email='claydavi@indiana.edu',
+      license='MIT',
+      packages=['botornot'],
+      install_requires=[
+          'requests',
+          'tweepy',
+          ],
+      )
