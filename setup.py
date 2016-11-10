5aad468f48ab562f90b49fbeabab544f35c8e263
Clayton Davis <clayadavis@gmail.com>
2016-11-10 11:15:09 -0500

    Release 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='botornot',
-      version='0.3-alpha',
+      version='0.3',
       description='Check Twitter accounts for bot behavior',
       url='https://github.com/truthy/botornot-python',
       download_url='https://github.com/truthy/botornot-python/archive/0.3.zip',
