ae25c771d78f7edcc0d4339552e585c9188a910f
yangkc <yangkc@iu.edu>
2020-08-08 01:41:03 -0400

    add class to interact with BotometerLite API

--- botometer/__init__.py
+++ botometer/__init__.py
@@ -5,6 +5,7 @@ import requests
 from requests import ConnectionError, HTTPError, Timeout
 import tweepy
 from tweepy.error import RateLimitError, TweepError
+import datetime
 
 
 class NoTimelineError(ValueError):
@@ -169,3 +170,110 @@ class Botometer(object):
                 if result is not None:
                     yield account, result
                     break
+
+
+class BotometerLite(Botometer):
+    """
+    Class to interact with the BotometerLite API.
+    The present class has two modes: the Twitter mode and the non-Twitter mode.
+
+    Non-Twitter mode:
+        No Twitter APP key is required in this mode, i.e., only rapidapi_key is required.
+        The users provide a list of no more than 100 tweets that they want to perform
+        bot detection on.
+        Via the self.check_accounts_from_tweets method, the present class
+        queries the RapidAPI to fetch the botscores.
+
+    Twitter mode:
+        A valid Tiwtter APP key is required in this mode, i.e., consumer_key, consumer_secret,
+        and rapidapi_key are required.
+        The users provide a list of no more than 100 user_ids or screen_names.
+        Via the self.check_accounts_from_user_ids or check_accounts_from_screen_names method,
+        the the present class first queries the Twitter user lookup API to fetch the user profiles,
+        then queries the RapidAPI to fetch the botscore.
+
+    Lists of tweets/user_ids/screen_names with more than 100 elements would be truncated to 100.
+    The users are responsible to handle the exceptions.
+    """
+    def __init__(
+        self,
+        rapidapi_key=None,
+        consumer_key=None,
+        consumer_secret=None,
+        access_token=None,
+        access_token_secret=None,
+        **kwargs
+    ):
+        self.twitter_mode = False
+        if consumer_key is None or consumer_secret is None:
+        # No Twitter mode: the users provide the tweets
+            self.api_url = kwargs.get(
+                'botometer_api_url',
+                'https://botometer15.p.rapidapi.com'
+            )
+            self.api_version = kwargs.get(
+                'botometer_api_version',
+                'litev1'
+            )
+        else:
+        # Twitter mode: use Twitter lookup to fetch user profile
+            self.twitter_mode = True
+            super(BotometerLite, self).__init__(
+                consumer_key,
+                consumer_secret,
+                access_token=access_token,
+                access_token_secret=access_token_secret,
+                botometer_api_version='litev1',
+                wait_on_ratelimit=kwargs.get('wait_on_ratelimit', False)
+            )
+
+        self.rapidapi_key = rapidapi_key or kwargs.get('mashape_key')
+
+    def _get_utc_now(self):
+        now_time = datetime.datetime.now(datetime.timezone.utc)
+        return datetime.datetime.strftime(
+            now_time, '%a %b %d %H:%M:%S %z %Y'
+        )
+
+    def _get_twitter_data(self, **kwargs):
+        try:
+            user_objs = self.twitter_api.lookup_users(**kwargs)
+        except RateLimitError as e:
+            e.args = (self._TWITTER_RL_MSG, 'users/lookup')
+            raise e
+        return user_objs
+
+    def _check_accounts_twitter_mode(self, is_id, ids_or_names, **kwargs):
+        assert self.twitter_mode, "Twitter app key missing"
+
+        ids_or_names = list(ids_or_names)[:100]
+
+        if is_id:
+            query = {"user_ids": ids_or_names}
+        else:
+            query = {"screen_names": ids_or_names}
+
+        user_objs = self._get_twitter_data(**query)
+        now_str = self._get_utc_now()
+
+        dummy_tweets = []
+        for user_obj in user_objs:
+            dummy_tweets.append({
+                'created_at': now_str,
+                'user': user_obj
+            })
+        return self.check_accounts_from_tweets(dummy_tweets)
+
+    def check_accounts_from_user_ids(self, user_ids, **kwargs):
+        return self._check_accounts_twitter_mode(True, user_ids, **kwargs)
+
+    def check_accounts_from_screen_names(self, screen_names, **kwargs):
+        return self._check_accounts_twitter_mode(False, screen_names, **kwargs)
+
+    def check_accounts_from_tweets(self, tweets):
+        url = self.bom_api_path('check_accounts_in_bulk')
+        bom_resp = self._bom_post(url, json=tweets)
+        bom_resp.raise_for_status()
+        classification = bom_resp.json()
+
+        return classification
